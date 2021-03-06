34272c875ef012a610cd605e831528636773630f
Antonio985 <ptoo.49ab@gmail.com>
2021-03-06 15:18:19 -0700

    Update Actividad_7_For.py
    Ejermplo sobre estructura for - basico

--- Actividad_7_For.py
+++ Actividad_7_For.py
@@ -1,5 +1,8 @@
 #Actividad 7 Ejercicios For
-
+numeros = [1, 2, 3, 4, 5, 6, 7, 8 , 9, 10]
+numeros2 = [1, 2, 3, 4, 5, 6, 7, 8 , 9, 10]
+cadena="BIENVENIDOS"
+indice=0
 for letra in "UNIVERSIDAD ESTATAL DE SONORA":
     print(letra)
 else:
@@ -7,7 +10,33 @@ else:
     
     
 
-numeros= [1, 2, 3, 4, 5, 6, 7, 8 , 9, 10]
-#Recorrer lista con un for
-    for i in numeros:
-        print(i)
+
+for i in numeros:
+    print(i)
+    print("")
+
+for x in numeros:
+    x*=20
+    print(x)
+    
+
+for x in numeros:
+    numeros[indice]*=20
+    indice+=1    
+    
+    print(numeros)
+    print("")
+    print("")
+    
+for indice, numero in enumerate(numeros2):
+    numeros2[indice]*=40
+   
+    print(numeros2)
+    print("")
+
+    
+for caracter in cadena:
+    print(caracter)
+    
+    
+    
