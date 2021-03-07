f8b2c042f24c7224743bf268f15047b845eb07cd
Armenta1702 <armenta.jenn15@gmail.com>
2021-03-06 21:13:29 -0700

    Armenta1702
    adicion de funciones

---
+++ funcion_argumentos.py
@@ -0,0 +1,6 @@
+#Funcion con parametros
+def funcion_arg(nombre):
+    print("El nombre que hemos recivido es: ",nombre)
+    
+#Invocamos la funcion
+funcion_arg("JENNYFER")
---
+++ funciones.py
@@ -0,0 +1,6 @@
+#Sintaxis basica de una funcion
+def mi_funcion():
+    print("Ejecucion de la funcion")
+    
+#Llamando la funcion
+mi_funcion()
