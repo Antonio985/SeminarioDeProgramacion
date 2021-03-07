ac5ec022c82bd9704f595dc43c3bfe582706bdd1
Antonio985 <ptoo.49ab@gmail.com>
2021-03-06 22:41:45 -0700

    Update funcion_argumentos.py
    correccion ortografica

--- funcion_argumentos.py
+++ funcion_argumentos.py
@@ -1,6 +1,6 @@
 #Funcion con parametros
 def funcion_arg(nombre):
-    print("El nombre que hemos recivido es: ",nombre)
+    print("El nombre que hemos recibido es: ",nombre)
     
 #Invocamos la funcion
-funcion_arg("JENNYFER")
+funcion_arg("Antonio")
