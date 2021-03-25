70b55de1e47478b026b14f164da49b4e56a4aaeb
Antonio985 <ptoo.49ab@gmail.com>
2021-03-24 17:21:48 -0700

    creacion clase base empleado

---
+++ 2doElemento/polimorfismo/empleados.py
@@ -0,0 +1,12 @@
+#creamos clase
+class  Empleado:
+    def __init__(self, nombre, sueldo):
+        self.nombre = nombre
+        self.sueldo = sueldo
+    
+    #enviamos a impresion
+    def __str__(self):
+        return "Nombre : "+ self.nombre + " "+ self.sueldo
+
+
+        
